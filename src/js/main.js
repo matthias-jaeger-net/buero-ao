c733c3ca2622f2555d61016811065f393ae1ed44
Matthias Jäger <write@matthias-jaeger.net>
2019-04-11 14:51:56 +0200

    not yet working but structure is ok, need to do the vector math now :(

--- src/js/main.js
+++ src/js/main.js
@@ -11,16 +11,59 @@
  * Graz, 2019
  */
 
-let midpoint;
+// DOM ELEMEMENTS
+var logos;
+var logo;
+var canvas;
 
+// PROGRAM VARIABLES
+var midpoint;
+var direction;
+var mousepos;
+
+/** Stores the projected points */
+var projection = [];
+
+// EXECUTES ONCE AND BEFORE DRAW
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, windowHeight);
+  logos = select('#logos');
+  canvas.parent(logos);
+
+  for (var i = 0; i < 10; i++) {
+    logo = createElement('div');
+    logo.id('logo' + i);
+    logo.class('logo');
+    logo.parent(logos);
+    logo.position(windowWidth / 2 - 250, windowHeight / 2 - 250);
+    projection.push(logo);
+  }
+
+  // for (var i = 0; i < 10; i++) {
+  //   var t = select('#logo' + i);
+  //   t.position(random(windowWidth), random(windowHeight));
+  // }
+
   midpoint = createVector(width * 0.5, height * 0.5);
+  direction = createVector(0.0, 0.0);
+  mousepos = createVector(0.0, 0.0);
 }
 
+// EXECUTES EVERY FRAME
 function draw() {
-  background(255);
-  strokeWeight(4);
-  point(midpoint.x, midpoint.y);
-  line(midpoint.x, midpoint.y, mouseX, mouseY);
+  background("plum");
+  if (mouseX != 0) {
+    mousepos.set(mouseX, mouseY);
+    direction = mousepos.sub(midpoint).normalize();
+
+    point(midpoint.x, midpoint.y);
+
+    midpoint.add(direction);
+    line(width / 2, height / 2, midpoint.x, midpoint.y)
+
+    for (var i = 0; i < projection.length; i++) {
+      var t = select('#logo' + i);
+      t.position((midpoint.x - 250), midpoint.y - 250);
+    }
+  }
 }
